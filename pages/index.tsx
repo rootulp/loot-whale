68c8409877265c67af8802d46d882d54810226c3
Rootul Patel <rootulp@gmail.com>
2022-02-26 03:22:00 -0500

    useENSName is flakey

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,11 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { GetServerSideProps } from 'next'
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { useENSName } from "use-ens-name";
 
 interface Wallet {
     address: string
     bagsHeld: string
-
 }
 
 interface HomeProps {
@@ -18,19 +18,19 @@ const Home: NextPage<HomeProps> = ({wallets}) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Loot Whale</title>
+        <title>Loot Whales</title>
         <meta name="description" content="A table of Loot whales" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐳</text></svg>"></link>
       </Head>
 
       <h1 className={styles.title}>
-          Loot Whale
+        🐳 Loot Whales
       </h1>
       <main className={styles.main}>
-        <table style={{ width: 500 }}>
+        <table style={{ minWidth: 600 }}>
                 <thead>
                     <tr>
-                        <th>Index</th>
+                        <th>Position</th>
                         <th>Address</th>
                         <th>Bags Held</th>
                     </tr>
@@ -39,7 +39,7 @@ const Home: NextPage<HomeProps> = ({wallets}) => {
                 {wallets.map((wallet, index) => {
                     return (<tr key={wallet.address}>
                         <td>{index + 1}</td>
-                        <td>{wallet.address}</td>
+                        <td>{useENSName(wallet.address)}</td>
                         <td>{wallet.bagsHeld}</td>
                     </tr>)
                 })}
@@ -51,16 +51,16 @@ const Home: NextPage<HomeProps> = ({wallets}) => {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    const SUBGRAPH = 'https://api.thegraph.com/subgraphs/name/shahruz/loot'
+    const subgraph = 'https://api.thegraph.com/subgraphs/name/shahruz/loot'
     const client = new ApolloClient({
-        uri: SUBGRAPH,
+        uri: subgraph,
         cache: new InMemoryCache()
     });
 
     const { data } = await client.query({
         query: gql`
             {
-                wallets(first: 100, orderBy: bagsHeld, orderDirection: desc) {
+                wallets(first: 25, orderBy: bagsHeld, orderDirection: desc) {
                     address
                     bagsHeld
                 }
